4ab1fa48357454b544ad74a3b825e85914092529
Boyun Tang <tangboyun@hotmail.com>
2012-10-25 17:14:42 +0800

    first commit

---
+++ src/Bio/Seq/EMBL/Parser.hs
@@ -0,0 +1,17 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module : Parser for EMBL format
+-- Copyright : (c) 2012 Boyun Tang
+-- License : BSD-style
+-- Maintainer : tangboyun@hotmail.com
+-- Stability : experimental
+-- Portability : ghc
+-- Reference : ftp://ftp.ebi.ac.uk/pub/databases/embl/release/usrman.txt
+-- 
+--
+-----------------------------------------------------------------------------
+
+module Bio.Seq.EMBL.Parser
+
+       where
+
