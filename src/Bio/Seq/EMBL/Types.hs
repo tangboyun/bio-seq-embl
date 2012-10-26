f643e40721b8d7226a07113a47cf550e71c463e4
Boyun Tang <tangboyun@hotmail.com>
2012-10-26 17:14:14 +0800

    first commit

--- src/Bio/Seq/EMBL/Types.hs
+++ src/Bio/Seq/EMBL/Types.hs
@@ -1,19 +1,17 @@
 -----------------------------------------------------------------------------
 -- |
--- Module : Parser for EMBL format
+-- Module : 
 -- Copyright : (c) 2012 Boyun Tang
 -- License : BSD-style
 -- Maintainer : tangboyun@hotmail.com
 -- Stability : experimental
 -- Portability : ghc
--- Reference : ftp://ftp.ebi.ac.uk/pub/databases/embl/release/usrman.txt
+--
 -- 
 --
 -----------------------------------------------------------------------------
 
-module Bio.Seq.EMBL.Parser
+module Bio.Seq.EMBL.Types
 
        where
-
-import Data.Attoparsec.ByteString.Char8
 
