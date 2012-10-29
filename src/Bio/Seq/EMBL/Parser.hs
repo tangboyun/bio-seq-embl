5d92608f843bb153c328d315ff2a007d72f843fe
Boyun Tang <tangboyun@hotmail.com>
2012-10-29 17:27:26 +0800

    unfinished lineKW

--- src/Bio/Seq/EMBL/Parser.hs
+++ src/Bio/Seq/EMBL/Parser.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module : Parser for EMBL format
@@ -16,4 +17,86 @@ module Bio.Seq.EMBL.Parser
        where
 
 import Data.Attoparsec.ByteString.Char8
+import qualified Data.ByteString.Char8 as B8
+import Control.Applicative
+import Data.Time.Format
+import System.Locale
+import Data.Time
+import Data.Maybe
 
+lineID = do
+  _ <- "ID" .*> count 3 (satisfy (== ' ')) <?> "ID Line"
+  t1 <- takeWhile1 (/= ';') <* char ';' <?> "Primary accession number" 
+  skipSpace
+  t2 <- "SV" .*> skipSpace *> (decimal <?> "Sequence version number") <* char ';' 
+  t3 <- skipSpace *> ((string "linear" <|> string "circular") <?> "Topology") <* char ';'
+  t4 <- skipSpace *> takeWhile1 (/= ';') <* char ';' <?> "Molecule type"
+  t5 <- skipSpace *>
+        (string "CON" <|> -- Entry constructed from segment entry sequences; if unannotated,
+                          -- annotation may be drawn from segment entries
+         string "PAT" <|> -- Patent
+         string "EST" <|> -- Expressed Sequence Tag
+         string "GSS" <|> -- Genome Survey Sequence
+         string "HTC" <|> -- High Thoughput CDNA sequencing
+         string "HTG" <|> -- High Thoughput Genome sequencing
+         string "MGA" <|> -- Mass Genome Annotation
+         string "WGS" <|> -- Whole Genome Shotgun
+         string "TSA" <|> -- Transcriptome Shotgun Assembly
+         string "STS" <|> -- Sequence Tagged Site
+         string "STD" <?> -- Standard (all entries not classified as above)
+         "Data Class"
+        ) <* char ';'
+  t6 <- skipSpace *>
+        (string "PHG" <|> -- Bacteriophage
+         string "ENV" <|> -- Environmental Sample    
+         string "FUN" <|> -- Fungal      
+         string "HUM" <|> -- Human
+         string "INV" <|> -- Invertebrate             
+         string "MAM" <|> -- Other Mammal            
+         string "VRT" <|> -- Other Vertebrate         
+         string "MUS" <|> -- Mus musculus
+         string "PLN" <|> -- Plant
+         string "PRO" <|> -- Prokaryote
+         string "ROD" <|> -- Other Rodent
+         string "SYN" <|> -- Synthetic
+         string "TGN" <|> -- Transgenic
+         string "UNC" <|> -- Unclassified
+         string "VRL" <?> -- Viral
+         "Taxonomic Division"
+        ) <* char ';'
+  t7 <- skipSpace *> (decimal <?> "Sequence Length") <* skipSpace <*. "BP." <* endOfLine
+  undefined
+  
+lineAC = do
+  _ <- "AC" .*> count 3 (satisfy (== ' ')) <?> "AC Line"
+  (takeWhile1 (/= ';') <* char ';') `sepBy1` (many1 $ char ' ') <* endOfLine
+
+linePR = do
+  _ <- "PR" .*> count 3 (satisfy (== ' ')) <?> "PR Line"
+  "Project:" .*> (decimal <?> "Project Number") <* char ';' <* endOfLine
+
+toTime :: String -> Maybe UTCTime
+toTime = parseTime defaultTimeLocale "%d-%b-%Y"
+
+parseDT = do
+  _ <- "DT" .*> count 3 (satisfy (== ' ')) <?> "DT Line"
+  takeWhile1 (/= ' ') <* skipSpace
+
+lineDT1 = do
+  str1 <- fmap B8.unpack parseDT
+  relNum <- "(Rel." .*> skipSpace *> decimal <* char ',' <* skipSpace <*. "Created)" <* endOfLine
+  return (fromJust $! toTime str1,relNum)
+  
+lineDT2 = do
+  str2 <- fmap B8.unpack parseDT
+  relNum <- "(Rel." .*> skipSpace *> decimal <* char ',' <* skipSpace <*. "Last updated,"
+  verNum <- skipSpace *> "Version" .*> skipSpace *> decimal <* char ')' <* endOfLine
+  return (fromJust $! toTime str2,relNum,verNum)
+
+lineDE = do
+  _ <- "DE" .*> count 3 (satisfy (== ' ')) <?> "DE Line"
+  takeWhile1 (/= '\n') <* endOfLine
+
+lineKW = do
+  _ <- "KW" .*> count 3 (satisfy (== ' ')) <?> "KW Line"
+  takeWhile1 (\c -> c /= ';' || c /= '.') `sepBy1` string "; " <* char '.' <* endOfLine
