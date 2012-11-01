89684c33c25a07e63078896696eed88018af8412
Boyun Tang <tangboyun@hotmail.com>
2012-11-01 17:28:48 +0800

    finish RefLoc

--- src/Bio/Seq/EMBL/Parser.hs
+++ src/Bio/Seq/EMBL/Parser.hs
@@ -16,16 +16,17 @@ module Bio.Seq.EMBL.Parser
 
        where
 
-import Data.Attoparsec.ByteString.Char8 hiding (isSpace)
+import           Control.Applicative
+import           Data.Attoparsec.ByteString.Char8
+import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as B8
-import Data.ByteString (ByteString)
-import Control.Applicative
-import Data.Time.Format
-import System.Locale
-import Data.Time
-import Data.Char (isSpace)
-import Data.Maybe
+import           Data.Maybe
+import           Data.Time
+import           Data.Time.Format
+import           System.Locale
+import Bio.Seq.EMBL.Types
 import Prelude hiding (takeWhile)
+
 lineID = do
   _ <- "ID" .*> count 3 (satisfy (== ' ')) <?> "ID Line"
   t1 <- takeWhile1 (/= ';') <* char ';' <?> "Primary accession number" 
@@ -118,8 +119,9 @@ lineDE = do
 
 lineKW = do
   _ <- "KW" .*> count 3 (satisfy (== ' ')) <?> "KW Line"
-  ss <- takeWhile1 (\c -> c /= ';' && c /= '.' && c/= '\n') `sepBy`
+  ss <- takeWhile1 (\c -> c /= ';' && c /= '.' && c /= '\n') `sepBy`
         string "; " <?> "Keywords sepBy \"; \""
+  _ <- option "" $ string ";"
   return ss
   
 parseKW = do
@@ -128,12 +130,210 @@ parseKW = do
 parseOS = do
   _ <- "OS" .*> count 3 (satisfy (== ' ')) <?> "OS Line"
   des <- fmap (B8.intercalate " ") $
-         takeWhile1 (\c -> c /= '(' && c /= '\n') `sepBy1` (endOfLine <*. "OS   ")
-  name <- option Nothing $ -- can handle "OS   Trifolium repens\nOS   (white\nOS   clover)\n"
+         takeWhile1 (\c -> c /= '(' && c /= '\n') `sepBy1`
+         (endOfLine <*. "OS   ")
+  name <- option Nothing $
+          -- can handle "OS   Trifolium repens\nOS   (white\nOS   clover)\n"
           fmap (Just . B8.intercalate " ") $
-          ((endOfLine *> "OS   (" .*> (takeWhile1 (\c -> c /= ')' && c /= '\n') `sepBy1`
-                                       (endOfLine <*. "OS   ")) <* char ')') <|>
+          ((endOfLine *> "OS   (" .*>
+            (takeWhile1 (\c -> c /= ')' && c /= '\n') `sepBy1`
+             (endOfLine <*. "OS   ")) <* char ')') <|>
            (char '(' *> (takeWhile1 (\c -> c /= ')' && c /= '\n') `sepBy1`
                          (endOfLine <*. "OS   ")) <* char ')'))
   endOfLine
   return (des,name)
+
+lineOC = do
+  _ <- "OC" .*> count 3 (satisfy (== ' ')) <?> "OC Line"
+  ss <- takeWhile1 (\c -> c /= ';' && c /= '.' && c /= '\n') `sepBy`
+        string "; " <?> "Organism Classification sepBy \"; \""
+  _ <- option "" $ string ";"
+  return ss
+
+parseOC = do
+  fmap concat (lineOC `sepBy1` char '\n') <* char '.' <* endOfLine  
+
+lineOG = do
+  "OG" .*> count 3 (satisfy (== ' ')) *>
+    takeWhile1 (/= '\n') <*
+    endOfLine <?> "OG Line"
+
+lineRN = do
+  "ON" .*> count 3 (satisfy (== ' ')) *> char '[' *>
+    decimal <*
+    char ']' <* endOfLine <?> "RN Line"
+
+lineRC = do
+  "RC" .*> count 3 (satisfy (== ' ')) *>
+    takeWhile1 (/= '\n') <?> "RC Line"
+
+parseRC = do
+  comments <- lineRC `sepBy1` endOfLine
+  endOfLine
+  return $! B8.intercalate " " comments
+
+lineRP = do
+  "RP" .*> count 3 (satisfy (== ' ')) *>
+    (do
+        beg <- decimal
+        _ <- char '-'
+        end <- decimal
+        return (beg,end)
+    ) `sepBy1` string ", "
+
+parseRP =
+  lineRP `sepBy1` endOfLine <* endOfLine
+
+parseResource = parsePUBMED <|>
+                parseDOI <|>
+                parseAGRICOLA
+  where
+    parsePUBMED = fmap PUBMED
+                  (string "PUBMED" *> string "; " *>
+                   (takeWhile1 isDigit <?> "Invalid PMID") <*
+                   char '.')
+    parseDOI = fmap (DOI . B8.init)
+               (string "DOI" *> string "; " *>
+                (takeWhile1 (/= '\n')))
+    parseAGRICOLA = fmap (AGRICOLA . B8.init)
+                    (string "AGRICOLA" *> string "; " *>
+                     (takeWhile1 (/= '\n')))
+
+lineRX = do
+  ("RX" .*> count 3 (satisfy (== ' ')) *>
+   parseResource <?> "RX Line") <* endOfLine 
+  
+lineRG = do
+  ("RG" .*> count 3 (satisfy (== ' ')) <?> "RG Line") *>
+    takeWhile1 (/= '\n') <* endOfLine
+
+lineRA = do
+  ("RA" .*> count 3 (satisfy (== ' ')) <?> "RA Line") *>
+    (takeWhile1 (\c -> c /= ',' && c /= '\n' && c /= ';') `sepBy1` string ", ")
+
+parseRA = do
+  fmap concat $ (lineRA `sepBy1` string ",\n") <* char ';' <* endOfLine
+
+lineRT = do
+  "RT" .*> count 3 (satisfy (== ' ')) <?> "RT Line"
+    
+parseRT = do
+  fmap (trim . B8.intercalate " ") $ -- trim for rare case "RT   \"Title\nRT   \";\n"
+    (
+      lineRT *> char '"' *>
+      (fmap (:[]) $ takeWhile1 $ \c -> c /= '"' && c /= '\n' && c /= ';')
+      <* char '"' <|>  -- one line title 
+      (do
+          line1 <- lineRT *> char '"' *> takeWhile1 (/= '\n') <* endOfLine <* lineRT
+          ls <- takeWhile (\c -> c /= '\n' && c /= '"') `sepBy1` (endOfLine *> lineRT)
+          _ <- char '"'
+          return $ line1 : ls) <|> -- multi-line title
+      lineRT *> return []  -- empty title
+    ) <* char ';' <* endOfLine
+       
+
+parseRL :: Parser RefLoc
+parseRL = do
+  parsePaper <|>
+  parseSubmitted <|>
+  parsePatent <|>
+  parseUnpublished <|>
+  parseMisc <|>
+  parseBook <|>
+  parseThesis
+  where
+    lineRL = do
+      "RL" .*> count 3 (satisfy (== ' ')) <?> "RL Line"
+    parsePaper = do
+      (pub,vol,iss,pBeg,pEnd,y) <- parseJ
+      return $ Paper pub vol iss (pBeg,pEnd) y
+    parseMisc = do
+      (pub,vol,iss,pBeg,pEnd,y) <- lineRL *> "(misc) " .*> parseJ
+                                   return $ Misc pub vol iss (pBeg,pEnd) y
+  
+    parseJ = do
+      pub <- fmap (Publication . B8.intercalate " ") $
+             word `sepBy1` char ' '
+      vol <- char ' ' *> decimal
+      iss <- option Nothing (fmap Just $ char '(' *> decimal <* char ')')
+             pBeg <- char ':' *> decimal <* char '-'
+      pEnd <- decimal
+      y    <- char '(' *> decimal <* char ')' <* char '.' <* endOfLine
+              return (pub,vol,iss,pBeg,pEnd,y)
+      where 
+        word = takeWhile1
+               (\c -> isAlpha_ascii c || isDigit c || c == '.') <*
+               (do
+                   c1 <- peekChar
+                   case c1 of
+                     Just ' ' -> return ()
+                     _ -> fail "Not word boundary")
+  
+    parseBook = do
+      auths <- fmap (map Author . concat) $
+               lineRL *> "(in) " .*>
+               (as `sepBy1` (char ',' *> endOfLine *> lineRL)) <*
+               char ';' <* endOfLine
+      bookName <- fmap (Publication . B8.intercalate " ") $
+                  lineRL *> (takeWhile (\c -> c /= ':' && c /= '\n') `sepBy1`
+                           (endOfLine <* lineRL)) <* char ':'
+      pBeg <- decimal <* char '-'
+      pEnd <- decimal <* char ';' <* endOfLine
+      puber <- fmap (Publisher . trim . B8.intercalate " ") $
+               lineRL *>
+               takeWhile (\c -> c /= '(' && c /= '\n') `sepBy1`
+               (endOfLine <* lineRL)
+      y <- char '(' *> decimal <* char ')' <* char '.' <* endOfLine
+      return $ Book bookName auths puber (pBeg,pEnd) y
+      where
+        as = takeWhile (\c -> c /= ',' && c /= ';') `sepBy1` string ", "
+    
+      parseSubmitted = do
+        timeStr <- fmap B8.unpack $
+                   lineRL *> "Submitted " .*> char '(' *>
+                   takeWhile1 (/= ')') <* char ')'
+        db <- fmap Database $ " to the " .*>
+              takeWhile1 (/= ' ') <*. " database" <*
+              option "" (string "s") <* char '.' <* endOfLine
+        addr <- option Nothing $ fmap (Just . Address . B8.intercalate " ") $
+                (lineRL *> takeWhile1 (/= '\n')) `sepBy1` endOfLine
+        case toTime timeStr of
+          Nothing -> fail "Not a valid time string"
+          Just t -> return $ Submitted t db addr
+
+      parseThesis = do
+        y <- lineRL *> "Thesis " .*>
+             char '(' *> decimal <* char ')' <*. ", "
+        sch <- fmap (Address . B8.intercalate " ") $
+               takeWhile1 (/= '\n') `sepBy1` (endOfLine <* lineRL)
+        endOfLine
+        return $ Thesis sch y
+
+      parseUnpublished = do
+        lineRL *> "Unpublished" .*>
+          char '.' *> endOfLine *> return Unpublished
+
+      parsePatent = do
+        patNum <- fmap ApplicationNumber $
+                  lineRL *>
+                  "Patent number " .*> takeWhile1 (/= '-') <* char '-'
+        patType <- fmap PatentType $ takeWhile1 (/= '/') <* char '/'
+        sn <- decimal <*. ", "
+        tStr <- fmap B8.unpack $
+                takeWhile1 (/= '.') <* char '.' <* endOfLine
+        appCan <- fmap (Applicant . B8.intercalate " ") $
+                  (lineRL *> takeWhile (/= '\n')) `sepBy1` endOfLine
+        endOfLine
+        case toTime tStr of
+          Nothing -> fail "Not a valid time string"
+          Just t -> return $ Patent patNum patType sn t appCan
+
+
+-- [65,66,67,68,71,72,75,77,78,82,83,84,86,87,89]
+isIUPAC :: Word8 -> Bool
+isIUPAC w | w > 121 = False
+          | w > 89 = isIUPAC (w - 32)
+          | w < 65 = False        
+          | otherwise =
+            case w of
+              
--- src/Bio/Seq/EMBL/Types.hs
+++ src/Bio/Seq/EMBL/Types.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GADTs #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module : 
@@ -15,3 +16,68 @@ module Bio.Seq.EMBL.Types
 
        where
 
+import Data.ByteString (ByteString)
+import Data.Time
+
+data Resource = PUBMED ByteString
+              | DOI ByteString
+              | AGRICOLA ByteString
+              deriving (Show,Eq)
+                       
+data RefLoc where
+  Paper ::
+    { journal :: Publication
+    , volume :: {-# UNPACK #-} !Int
+    , issue :: Maybe Int
+    , pages :: (Int,Int)
+    , year :: {-# UNPACK #-} !Int
+    } -> RefLoc
+  Book ::
+    { book :: Publication
+    , authors :: [Author]
+    , publisher :: Publisher
+    , pages :: (Int,Int)
+    , year :: {-# UNPACK #-} !Int
+    } -> RefLoc
+  Submitted ::
+    { date :: UTCTime
+    , database :: Database
+    , address :: Maybe Address
+    } -> RefLoc
+  Unpublished :: RefLoc
+  Thesis ::
+    { school :: Address
+    , year :: {-# UNPACK #-} !Int
+    } -> RefLoc
+  Patent ::
+    { patentNumber :: ApplicationNumber
+    , patentType :: PatentType
+    , serialNumber :: Int
+    , date :: UTCTime
+    , applicant :: Applicant
+    } -> RefLoc
+  Misc ::
+    { journal :: Publication
+    , volume :: {-# UNPACK #-} !Int
+    , issue :: Maybe Int
+    , pages :: (Int,Int)
+    , year :: {-# UNPACK #-} !Int
+    } -> RefLoc
+   deriving (Show,Eq)
+  
+newtype Author = Author ByteString
+               deriving (Show,Eq)
+newtype Publisher = Publisher ByteString
+                  deriving (Show,Eq)
+newtype Publication = Publication ByteString
+                    deriving (Show,Eq)
+newtype Address = Address ByteString
+                deriving (Show,Eq)
+newtype Database = Database ByteString
+                 deriving (Show,Eq)
+newtype ApplicationNumber = ApplicationNumber ByteString
+                          deriving (Show,Eq)
+newtype Applicant = Applicant ByteString
+                  deriving (Show,Eq)
+newtype PatentType = PatentType ByteString
+                   deriving (Show,Eq)
