c518844cc0caef476a6c41ffcefed9cd459ba26b
Boyun Tang <tangboyun@localhost.localdomain>
2012-11-01 21:15:04 +0800

    fast test for IUPAC

--- src/Bio/Seq/EMBL/Parser.hs
+++ src/Bio/Seq/EMBL/Parser.hs
@@ -24,8 +24,12 @@ import           Data.Maybe
 import           Data.Time
 import           Data.Time.Format
 import           System.Locale
+import           Data.Bits
+import           Data.Int
 import Bio.Seq.EMBL.Types
 import Prelude hiding (takeWhile)
+import Data.Word
+import Data.Char hiding (isSpace,isDigit)
 
 lineID = do
   _ <- "ID" .*> count 3 (satisfy (== ' ')) <?> "ID Line"
@@ -233,14 +237,14 @@ parseRT = do
        
 
 parseRL :: Parser RefLoc
-parseRL = do
-  parsePaper <|>
-  parseSubmitted <|>
-  parsePatent <|>
-  parseUnpublished <|>
-  parseMisc <|>
-  parseBook <|>
-  parseThesis
+parseRL =
+    parsePaper <|>
+    parseSubmitted <|>
+    parsePatent <|>
+    parseUnpublished <|>
+    parseMisc <|>
+    parseBook <|>
+    parseThesis
   where
     lineRL = do
       "RL" .*> count 3 (satisfy (== ' ')) <?> "RL Line"
@@ -249,17 +253,17 @@ parseRL = do
       return $ Paper pub vol iss (pBeg,pEnd) y
     parseMisc = do
       (pub,vol,iss,pBeg,pEnd,y) <- lineRL *> "(misc) " .*> parseJ
-                                   return $ Misc pub vol iss (pBeg,pEnd) y
+      return $ Misc pub vol iss (pBeg,pEnd) y
   
     parseJ = do
       pub <- fmap (Publication . B8.intercalate " ") $
              word `sepBy1` char ' '
       vol <- char ' ' *> decimal
       iss <- option Nothing (fmap Just $ char '(' *> decimal <* char ')')
-             pBeg <- char ':' *> decimal <* char '-'
+      pBeg <- char ':' *> decimal <* char '-'
       pEnd <- decimal
       y    <- char '(' *> decimal <* char ')' <* char '.' <* endOfLine
-              return (pub,vol,iss,pBeg,pEnd,y)
+      return (pub,vol,iss,pBeg,pEnd,y)
       where 
         word = takeWhile1
                (\c -> isAlpha_ascii c || isDigit c || c == '.') <*
@@ -288,52 +292,53 @@ parseRL = do
       where
         as = takeWhile (\c -> c /= ',' && c /= ';') `sepBy1` string ", "
     
-      parseSubmitted = do
-        timeStr <- fmap B8.unpack $
-                   lineRL *> "Submitted " .*> char '(' *>
-                   takeWhile1 (/= ')') <* char ')'
-        db <- fmap Database $ " to the " .*>
-              takeWhile1 (/= ' ') <*. " database" <*
-              option "" (string "s") <* char '.' <* endOfLine
-        addr <- option Nothing $ fmap (Just . Address . B8.intercalate " ") $
-                (lineRL *> takeWhile1 (/= '\n')) `sepBy1` endOfLine
-        case toTime timeStr of
-          Nothing -> fail "Not a valid time string"
-          Just t -> return $ Submitted t db addr
+    parseSubmitted = do
+      timeStr <- fmap B8.unpack $
+                 lineRL *> "Submitted " .*> char '(' *>
+                 takeWhile1 (/= ')') <* char ')'
+      db <- fmap Database $ " to the " .*>
+            takeWhile1 (/= ' ') <*. " database" <*
+            option "" (string "s") <* char '.' <* endOfLine
+      addr <- option Nothing $ fmap (Just . Address . B8.intercalate " ") $
+              (lineRL *> takeWhile1 (/= '\n')) `sepBy1` endOfLine
+      case toTime timeStr of
+        Nothing -> fail "Not a valid time string"
+        Just t -> return $ Submitted t db addr
 
-      parseThesis = do
-        y <- lineRL *> "Thesis " .*>
-             char '(' *> decimal <* char ')' <*. ", "
-        sch <- fmap (Address . B8.intercalate " ") $
-               takeWhile1 (/= '\n') `sepBy1` (endOfLine <* lineRL)
-        endOfLine
-        return $ Thesis sch y
+    parseThesis = do
+      y <- lineRL *> "Thesis " .*>
+           char '(' *> decimal <* char ')' <*. ", "
+      sch <- fmap (Address . B8.intercalate " ") $
+             takeWhile1 (/= '\n') `sepBy1` (endOfLine <* lineRL)
+      endOfLine
+      return $ Thesis sch y
 
-      parseUnpublished = do
-        lineRL *> "Unpublished" .*>
-          char '.' *> endOfLine *> return Unpublished
+    parseUnpublished = do
+      lineRL *> "Unpublished" .*>
+        char '.' *> endOfLine *> return Unpublished
 
-      parsePatent = do
-        patNum <- fmap ApplicationNumber $
-                  lineRL *>
-                  "Patent number " .*> takeWhile1 (/= '-') <* char '-'
-        patType <- fmap PatentType $ takeWhile1 (/= '/') <* char '/'
-        sn <- decimal <*. ", "
-        tStr <- fmap B8.unpack $
-                takeWhile1 (/= '.') <* char '.' <* endOfLine
-        appCan <- fmap (Applicant . B8.intercalate " ") $
-                  (lineRL *> takeWhile (/= '\n')) `sepBy1` endOfLine
-        endOfLine
-        case toTime tStr of
-          Nothing -> fail "Not a valid time string"
-          Just t -> return $ Patent patNum patType sn t appCan
+    parsePatent = do
+      patNum <- fmap ApplicationNumber $
+                lineRL *>
+                "Patent number " .*> takeWhile1 (/= '-') <* char '-'
+      patType <- fmap PatentType $ takeWhile1 (/= '/') <* char '/'
+      sn <- decimal <*. ", "
+      tStr <- fmap B8.unpack $
+              takeWhile1 (/= '.') <* char '.' <* endOfLine
+      appCan <- fmap (Applicant . B8.intercalate " ") $
+                (lineRL *> takeWhile (/= '\n')) `sepBy1` endOfLine
+      endOfLine
+      case toTime tStr of
+        Nothing -> fail "Not a valid time string"
+        Just t -> return $ Patent patNum patType sn t appCan
 
 
--- [65,66,67,68,71,72,75,77,78,82,83,84,86,87,89]
 isIUPAC :: Word8 -> Bool
-isIUPAC w | w > 121 = False
-          | w > 89 = isIUPAC (w - 32)
-          | w < 65 = False        
-          | otherwise =
-            case w of
-              
+isIUPAC w | pred <= 25 = unsafeShiftL (1 :: Int32) (fromIntegral pred) .&. iupacMask /= 0
+          | otherwise = False
+  where pred = (w .|. 32) - 97
+        iupacMask :: Int32
+        iupacMask = 23999695
+        -- iupacMask = foldr1 (.|.) $
+        --             map (unsafeShiftL 1 . (\n -> n - 97) . ord . toLower) "ABCDGHKMNRSTVWY"
+
