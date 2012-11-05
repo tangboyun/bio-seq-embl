9d0ecd48d1b9216776b7622b5680d411a3a62507
Boyun Tang <tangboyun@hotmail.com>
2012-11-05 17:05:13 +0800

    defined SeqRecord

--- src/Bio/Seq/EMBL/Types.hs
+++ src/Bio/Seq/EMBL/Types.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, DisambiguateRecordFields #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module : 
@@ -24,6 +24,54 @@ data Resource = PUBMED ByteString
               | AGRICOLA ByteString
               deriving (Show,Eq)
                        
+data Topology = Circular
+              | Linear
+              deriving (Show,Eq)
+                       
+data SeqRecord where
+  SeqRecord ::
+    { accessionNumber :: ByteString
+    , sequenceVersion :: Int
+    , topology :: Topology
+    , moleculeType :: ByteString
+    , dataClass :: DataClass
+    , taxonomy :: Taxonomy
+    , seqlength :: Int
+    , accessions :: [ByteString]
+    , project :: Maybe ByteString
+    , createdTime :: UTCTime
+    , lastUpdatedTime :: UTCTime
+    , description :: ByteString
+    , keywords :: [Keyword]
+    , organism :: Organism
+    , references :: [Reference]
+    , dbCrossRef :: Maybe ByteString
+    , comment :: Maybe ByteString
+    , assemblyInformation :: Maybe [AssemblyInformation]
+    , features :: [Feature]
+    , seqdata :: SeqData
+    } -> SeqRecord
+    
+data Organism = Oranism
+  { name :: ByteString
+  , commonName :: Maybe ByteString
+  , classification :: [ByteString]
+  , organelle :: Maybe ByteString
+  } deriving (Show,Eq)
+  
+data Reference where
+  Reference ::
+    { refNo :: RefNo
+    , refComment :: Maybe RefComment
+    , refPosition :: Maybe [(Int,Int)]
+    , refSource :: Maybe Resource
+    , refGroup :: Maybe RefGroup
+    , refAuthors :: [Author]
+    , refTitle :: Title
+    , refLocation :: RefLoc
+    } -> Reference
+  deriving (Show,Eq)
+
 data RefLoc where
   Paper ::
     { journal :: Publication
@@ -32,6 +80,7 @@ data RefLoc where
     , pages :: (Int,Int)
     , year :: {-# UNPACK #-} !Int
     } -> RefLoc
+    
   Book ::
     { book :: Publication
     , authors :: [Author]
@@ -39,16 +88,20 @@ data RefLoc where
     , pages :: (Int,Int)
     , year :: {-# UNPACK #-} !Int
     } -> RefLoc
+    
   Submitted ::
     { date :: UTCTime
     , database :: Database
     , address :: Maybe Address
     } -> RefLoc
+    
   Unpublished :: RefLoc
+  
   Thesis ::
     { school :: Address
     , year :: {-# UNPACK #-} !Int
     } -> RefLoc
+    
   Patent ::
     { patentNumber :: PatentNumber
     , patentType :: PatentType
@@ -56,6 +109,7 @@ data RefLoc where
     , date :: UTCTime
     , applicant :: Applicant
     } -> RefLoc
+    
   Misc ::
     { journal :: Publication
     , volume :: {-# UNPACK #-} !Int
@@ -66,15 +120,22 @@ data RefLoc where
    deriving (Show,Eq)
 
 data SeqStatistic = SeqSta 
-  { seqlength :: {-# UNPACK #-} !Int
-  , numOfA :: {-# UNPACK #-} !Int
+  { numOfA :: {-# UNPACK #-} !Int
   , numOfC :: {-# UNPACK #-} !Int
   , numOfG :: {-# UNPACK #-} !Int
   , numOfT :: {-# UNPACK #-} !Int
   , numOfOthers :: {-# UNPACK #-} !Int
   } deriving (Show,Eq)
 
-
+newtype RefNo = RefNo Int
+              deriving (Show,Eq)
+newtype RefComment = RefComment ByteString
+                   deriving (Show,Eq)                  
+newtype RefGroup = RefGroup ByteString
+                 deriving (Show,Eq)
+                         
+newtype Title = Title ByteString
+              deriving (Show,Eq)                  
 newtype Keyword = Keyword ByteString
                 deriving (Show,Eq)                  
 newtype Author = Author ByteString
@@ -94,5 +155,63 @@ newtype Applicant = Applicant ByteString
 newtype PatentType = PatentType ByteString
                    deriving (Show,Eq)
 
-newtype SeqData = SeqData ByteString
+data SeqData = SQ SeqStatistic ByteString
+             | CN ByteString 
+             deriving (Show,Eq)
+
+data Feature = Feature Key Location [(Qualifier,Value)]
+               deriving (Show,Eq)
+                        
+newtype Key = Key ByteString
+            deriving (Show,Eq)
+newtype Location = Location ByteString
+                 deriving (Show,Eq)
+newtype Qualifier = Qualifier ByteString
+                  deriving (Show,Eq)
+newtype Value = Value ByteString
                 deriving (Show,Eq)
+                         
+newtype Constructed = Constructed ByteString
+                    deriving (Show,Eq)
+                      
+newtype DBCrossRef = DBCrossRef ByteString
+                     deriving (Show,Eq)
+
+data AssemblyInformation = AssemblyInformation
+  { localSpan :: (Int,Int)
+  , primaryIdentifier :: ByteString
+  , primarySpan :: Maybe (Int,Int)
+  , isComplementary :: Bool
+  } deriving (Show,Eq)
+
+data DataClass = CON -- ^ Entry constructed from segment entry sequences;
+                     -- ^ if unannotated, annotation may be drawn from
+                     -- ^ segment entries
+               | PAT -- ^ Patent
+               | EST -- ^ Expressed Sequence Tag
+               | GSS -- ^ Genome Survey Sequence
+               | HTC -- ^ High Thoughput CDNA sequencing
+               | HTG -- ^ High Thoughput Genome sequencing
+               | MGA -- ^ Mass Genome Annotation
+               | WGS -- ^ Whole Genome Shotgun
+               | TSA -- ^ Transcriptome Shotgun Assembly
+               | STS -- ^ Sequence Tagged Site
+               | STD -- ^ Standard (all entries not classified as above)
+               deriving (Show,Eq)
+
+data Taxonomy = PHG -- ^ Bacteriophage
+              | ENV -- ^ Environmental Sample
+              | FUN -- ^ Fungal
+              | HUM -- ^ Human
+              | INV -- ^ Invertebrate
+              | MAM -- ^ Other Mammal
+              | VRT -- ^ Other Vertebrate
+              | MUS -- ^ Mus musculus
+              | PLN -- ^ Plant
+              | PRO -- ^ Prokaryote
+              | ROD -- ^ Other Rodent
+              | SYN -- ^ Synthetic
+              | TGN -- ^ Transgenic
+              | UNC -- ^ Unclassified
+              | VRL -- ^ Viral
+              deriving (Show,Eq)
